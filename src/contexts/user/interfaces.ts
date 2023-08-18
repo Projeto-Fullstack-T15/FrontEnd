f64500136e3ffa65fd4ab15e82916711060a5cf8
Vinicius <viniciusribeiro.dev@hotmail.com>
2023-08-18 15:12:33 -0300

    feat: login context

--- src/contexts/user/interfaces.ts
+++ src/contexts/user/interfaces.ts
@@ -5,6 +5,7 @@ export interface IUserContextProps {
   createUser: (data: TCreateUser) => Promise<void>;
   updateUser: (data: TUpdateUser) => Promise<void>;
   deleteUser: () => Promise<void>;
+  login: (data: ILogin) => Promise<void>;
 }
 
 export interface IUserProviderProps {
@@ -14,3 +15,8 @@ export interface IUserProviderProps {
 export type TCreateUser = Omit<IUser, 'id' | 'announcements'>;
 
 export type TUpdateUser = Partial<TCreateUser>;
+
+export interface ILogin {
+  email: string;
+  password: string;
+}
