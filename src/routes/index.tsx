83c4f8b355d1c6368c9dcbd4acc418def9c911c4
Vinicius <viniciusribeiro.dev@hotmail.com>
2023-09-04 19:05:07 -0300

    feat: product detail page mobile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import { HomePage } from "../pages/Home";
-import LoginPage from "../pages/Login";
-import RegisterPage from "../pages/Register";
-import { AnnouncementProvider } from "../contexts/announces/announcementContext";
-import { ProfileAdminPage } from "../pages/HomeProfileAdmin";
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from '../pages/Home';
+import LoginPage from '../pages/Login';
+import RegisterPage from '../pages/Register';
+import { AnnouncementProvider } from '../contexts/announces/announcementContext';
+import { ProfileAdminPage } from '../pages/HomeProfileAdmin';
+import DetailAnnouncement from '../pages/DetailAnnouncement';
 
 export const RoutesMain = () => {
   return (
     <Routes>
       <Route
-        path="/home"
+        path='/home'
         element={
           <AnnouncementProvider value={undefined}>
             <HomePage />
           </AnnouncementProvider>
         }
       />
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/profileadmin" element={<ProfileAdminPage />} />
+      <Route path='/' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/profileadmin' element={<ProfileAdminPage />} />
+      <Route path='/product-detail' element={<DetailAnnouncement />} />
     </Routes>
   );
 };
