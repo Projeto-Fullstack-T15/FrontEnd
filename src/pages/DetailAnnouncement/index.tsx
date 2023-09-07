c1c01bf6f7ed22f611e7c60f6a3f25fafbf28f9a
Vinicius <viniciusribeiro.dev@hotmail.com>
2023-09-07 00:20:19 -0300

    fix: @vinicius/ajustes conflitos resolvidos

--- src/pages/DetailAnnouncement/index.tsx
+++ src/pages/DetailAnnouncement/index.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { toast } from "react-toastify";
-import { CommentsBox } from "../../components/CommentsBox";
-import ButtonComponents from "../../components/Global/Buttons";
-import FooterComponent from "../../components/Global/Footer";
-import HeaderComponents from "../../components/Header";
-import Announcement from "../../contexts/announces/interface";
-import { api } from "../../services/api";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { CommentsBox } from '../../components/CommentsBox';
+import ButtonComponents from '../../components/Global/Buttons';
+import FooterComponent from '../../components/Global/Footer';
+import HeaderComponents from '../../components/Header';
+import Announcement from '../../contexts/announces/interface';
+import { api } from '../../services/api';
 import {
   BackgroundBrandStyle,
   Container,
@@ -18,7 +18,7 @@ import {
   SectionPhotosStyle,
   SectionProductDetailStyle,
 } from './style';
-import { CommentsBox } from '../../components/CommentsBox';
+import { Comment } from '../../components/Comment';
 
 const DetailAnnouncement = () => {
   const { productId } = useParams();
@@ -56,10 +56,7 @@ const DetailAnnouncement = () => {
         <DivDetailsProduct>
           <SectionProductDetailStyle>
             <div className='image--product'>
-              <img
-                src={detailProduct.cover_image}
-                alt='imagem do anuncio'
-              />
+              <img src={detailProduct.cover_image} alt='imagem do anuncio' />
             </div>
             <SectionDetailsProductStyle>
               <h3>
@@ -71,9 +68,9 @@ const DetailAnnouncement = () => {
                   <span>{detailProduct.mileage / 1000}KM</span>
                 </div>
                 <p>
-                  {detailProduct.price.toLocaleString("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
+                  {detailProduct.price.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
                   })}
                 </p>
               </DivYearKMPriceStyle>
@@ -102,69 +99,68 @@ const DetailAnnouncement = () => {
                   </li>
                 );
               })} */}
-              <li className='photos'>
-                <img
-                  src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
-                  alt='fotos do carro'
-                />
-              </li>
-              <li className='photos'>
-                <img
-                  src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
-                  alt='fotos do carro'
-                />
-              </li>
-              <li className='photos'>
-                <img
-                  src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
-                  alt='fotos do carro'
-                />
-              </li>
-              <li className='photos'>
-                <img
-                  src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
-                  alt='fotos do carro'
-                />
-              </li>
-              <li className='photos'>
-                <img
-                  src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
-                  alt='fotos do carro'
-                />
-              </li>
-              <li className='photos'>
-                <img
-                  src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
-                  alt='fotos do carro'
-                />
-              </li>
-            </ul>
-          </SectionPhotosStyle>
-          <SectionAnnouncerStyle>
-            <div>
-              {detailProduct.account.user.name
-                .split(' ')
-                .map((elt) => elt[0])
-                .join('')}
-            </div>
-            <h4>{detailProduct.account.user.name}</h4>
-            <p>{detailProduct.account.user.description}</p>
-            <ButtonComponents
-              text='Ver todos anuncios'
-              $size='large'
-              $width='206px'
-              typeButton='button'
-              $type='big45'
-              onClick={handleRedirect}
-            />
-          </SectionAnnouncerStyle>
-        </div>
-      </DivDetailsProduct>
-
-      <section>
-        <CommentsBox/>
-      </section>
-
+                <li className='photos'>
+                  <img
+                    src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
+                    alt='fotos do carro'
+                  />
+                </li>
+                <li className='photos'>
+                  <img
+                    src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
+                    alt='fotos do carro'
+                  />
+                </li>
+                <li className='photos'>
+                  <img
+                    src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
+                    alt='fotos do carro'
+                  />
+                </li>
+                <li className='photos'>
+                  <img
+                    src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
+                    alt='fotos do carro'
+                  />
+                </li>
+                <li className='photos'>
+                  <img
+                    src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
+                    alt='fotos do carro'
+                  />
+                </li>
+                <li className='photos'>
+                  <img
+                    src='https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419'
+                    alt='fotos do carro'
+                  />
+                </li>
+              </ul>
+            </SectionPhotosStyle>
+            <SectionAnnouncerStyle>
+              <div>
+                {detailProduct.account.user.name
+                  .split(' ')
+                  .map((elt) => elt[0])
+                  .join('')}
+              </div>
+              <h4>{detailProduct.account.user.name}</h4>
+              <p>{detailProduct.account.user.description}</p>
+              <ButtonComponents
+                text='Ver todos anuncios'
+                $size='large'
+                $width='206px'
+                typeButton='button'
+                $type='big45'
+                onClick={handleRedirect}
+              />
+            </SectionAnnouncerStyle>
+          </div>
+        </DivDetailsProduct>
+        <section>
+          <CommentsBox />
+        </section>
+      </Container>
       <FooterComponent />
     </>
   ) : null;
