017045b234ddd50a518b80b4b0ce57cf62d7953d
Vinicius <viniciusribeiro.dev@hotmail.com>
2023-09-07 12:38:18 -0300

    fix: type

--- src/components/Form/Register/schema.ts
+++ src/components/Form/Register/schema.ts
@@ -1,31 +1,35 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
-export const registerSchema = yup.object().shape({
-  email: yup.string().required("Campo obrigatório").email("E-mail inválido"),
+const userSchema = yup.object({
+  name: yup.string().required('Campo obrigatório'),
+  cpf: yup.string().required('Campo obrigatório'),
+  birthday: yup.string().required('Campo obrigatório'),
+  description: yup.string().required('Campo obrigatório'),
+});
+
+const addressSchema = yup.object({
+  zip_code: yup.string().required('Campo obrigatório'),
+  state: yup.string().required('Campo obrigatório'),
+  city: yup.string().required('Campo obrigatório'),
+  street: yup.string().required('Campo obrigatório'),
+  number: yup.string().required('Campo obrigatório'),
+  complement: yup.string().optional(),
+});
+
+export const registerSchema = yup.object({
+  email: yup.string().required('Campo obrigatório').email('E-mail inválido'),
   password: yup
     .string()
-    .required("Campo obrigatório")
-    .min(8, "Mínimo 8 caracteres"),
+    .required('Campo obrigatório')
+    .min(8, 'Mínimo 8 caracteres'),
   confirmPassword: yup
     .string()
-    .oneOf([yup.ref("password"), null], "As senhas devem ser iguais"),
-  phone: yup.string().required("Campo obrigatório"),
+    .oneOf([yup.ref('password'), null], 'As senhas devem ser iguais'),
+  phone: yup.string().required('Campo obrigatório'),
   account_type: yup
     .string()
-    .required("Campo obrigatório")
-    .oneOf(["BUYER", "ANNOUNCER"], "Selecione uma opção válida"),
-  user: yup.object().shape({
-    name: yup.string().required("Campo obrigatório"),
-    cpf: yup.string().required("Campo obrigatório"),
-    birthday: yup.string().required("Campo obrigatório"),
-    description: yup.string().required("Campo obrigatório"),
-  }),
-  address: yup.object().shape({
-    zip_code: yup.string().required("Campo obrigatório"),
-    state: yup.string().required("Campo obrigatório"),
-    city: yup.string().required("Campo obrigatório"),
-    street: yup.string().required("Campo obrigatório"),
-    number: yup.string().required("Campo obrigatório"),
-    complement: yup.string().optional(),
-  }),
+    .required('Campo obrigatório')
+    .oneOf(['BUYER', 'ANNOUNCER'], 'Selecione uma opção válida'),
+  user: userSchema,
+  address: addressSchema,
 });
