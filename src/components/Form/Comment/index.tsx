6de7dd15ee85b9112e3f0b574506c8f1fb20ee22
Vinicius <viniciusribeiro.dev@hotmail.com>
2023-09-08 09:57:33 -0300

    fix: bug dos comentarios

--- src/components/Form/Comment/index.tsx
+++ src/components/Form/Comment/index.tsx
@@ -1,12 +1,12 @@
-import { useContext } from "react";
-import { CommentContext } from "../../../contexts/comments/commentsContext";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { ICommentRequest } from "../../../contexts/comments/interface";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { commentSchema } from "./shema";
-import { Input } from "../Input";
-import ButtonComponents from "../../Global/Buttons";
-import { DivButton } from "./style";
+import { useContext } from 'react';
+import { CommentContext } from '../../../contexts/comments/commentsContext';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { ICommentRequest } from '../../../contexts/comments/interface';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { commentSchema } from './shema';
+import { Input } from '../Input';
+import ButtonComponents from '../../Global/Buttons';
+import { DivButton } from './style';
 
 export const CommentForm = ({ announcementId }) => {
   const { createComment, getComment } = useContext(CommentContext);
@@ -19,26 +19,26 @@ export const CommentForm = ({ announcementId }) => {
     resolver: yupResolver(commentSchema),
   });
 
-  const submit: SubmitHandler<ICommentRequest> = (data) => {
+  const submit: SubmitHandler<ICommentRequest> = async (data) => {
     createComment(announcementId, data);
-    getComment(announcementId)
+    getComment(announcementId);
   };
 
   return (
     <form onSubmit={handleSubmit(submit)}>
       <Input
-        type="textarea"
-        placeholder="Digitar seu comentário"
-        register={register("text")}
+        type='textarea'
+        placeholder='Digitar seu comentário'
+        register={register('text')}
         error={errors.text}
-        withBackground="yes"
+        withBackground='yes'
       />
       <DivButton>
         <ButtonComponents
-          typeButton="submit"
-          text="Comentar"
-          $type="brand1"
-          $size="large"
+          typeButton='submit'
+          text='Comentar'
+          $type='brand1'
+          $size='large'
         />
       </DivButton>
     </form>
